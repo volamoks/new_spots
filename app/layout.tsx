484d98eeaaebccbfa1ca297f950dc67a08e3068c
Volamoks <komalov@me.com>
2025-04-02 08:54:55 +0500

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,28 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-import type React from "react";
-import { Providers } from "./providers";
-import { Toaster } from "@/components/ui/toaster";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/lib/auth";
+import './globals.css';
+import { Inter } from 'next/font/google';
+import type React from 'react';
+import { Providers } from './providers';
+import { Toaster } from '@/components/ui/toaster';
+import { Analytics } from '@vercel/analytics/react'; // Import Vercel Analytics
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/lib/auth';
 
-const inter = Inter({ subsets: ["latin", "cyrillic"] });
+const inter = Inter({ subsets: ['latin', 'cyrillic'] });
 
 export const metadata = {
-  title: "Store Spots Booking",
-  description: "Бронирование дополнительных зон продажи в магазинах",
+    title: 'Store Spots Booking',
+    description: 'Бронирование дополнительных зон продажи в магазинах',
 };
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const session = await getServerSession(authOptions);
-  return (
-    <html lang="ru">
-      <body className={`${inter.className} bg-gray-100`}>
-        <Providers session={session}>{children}</Providers>
-        <Toaster />
-      </body>
-    </html>
-  );
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+    const session = await getServerSession(authOptions);
+    return (
+        <html lang="ru">
+            <body className={`${inter.className} bg-gray-100`}>
+                <Providers session={session}>{children}</Providers>
+                <Toaster />
+                <Analytics /> {/* Add Vercel Analytics component */}
+            </body>
+        </html>
+    );
 }
-
